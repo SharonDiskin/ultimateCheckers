a370fba7ffb88450f8f970129ec69a27fa49b09f
SharonDiskin <40810071+SharonDiskin@users.noreply.github.com>
2020-10-08 23:48:22 +0300

    Update AskAnotherRound.cs

--- AskAnotherRound.cs
+++ AskAnotherRound.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +12,41 @@ namespace C20_Ex02
 {
     public partial class AskAnotherRound : Form
     {
-        public AskAnotherRound()
+        Damka m_DamkaBoard;
+        GameManager.eGameResult m_GameResult;
+
+        public delegate void AnotherGameEventHandler(bool answer, EventArgs args);
+        public event AnotherGameEventHandler OnAnotherRound;
+
+        public AskAnotherRound(Damka i_DamkaGame)
         {
             InitializeComponent();
+        }
+
+        public GameManager.eGameResult GameResult
+        {
+            get { return m_GameResult; }
+            set
+            {
+                m_GameResult = value;
+                changeLabel1AccordingToResult(m_GameResult);
+            }
+        }
+
+        private void changeLabel1AccordingToResult(GameManager.eGameResult i_GameResult)
+        {
+            if (i_GameResult == GameManager.eGameResult.Player1Win)
+            {
+                label1.Text = "Player 1 Won!";
+            }
+            else if (i_GameResult == GameManager.eGameResult.Player2Win)
+            {
+                label1.Text = "Player 2 Won!";
+            }
+            else
+            {
+                label1.Text = "Tie!";
+            }
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -34,17 +66,22 @@ namespace C20_Ex02
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            this.Close();
+            if (OnAnotherRound != null)
+            {
+                OnAnotherRound.Invoke(true, EventArgs.Empty);
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            m_DamkaBoard.Close();
+            this.Close();
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+ 
         }
     }
 }
